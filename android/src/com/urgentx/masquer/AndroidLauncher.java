e6bca72f236f6b5755e04615d82c9b4cd1ad72be
Barco <barcozaleta@gmail.com>
2016-08-30 16:35:38 +1200

    Basic game files, including starter class and object classes.

---
+++ android/src/com/urgentx/masquer/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.urgentx.masquer;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.urgentx.masquer.Masquer;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+		initialize(new Masquer(), config);
+	}
+}
